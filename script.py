d097afca64feee055a9ae44eacabc2363581dbfd
OK2AMA <jakubhoferek@gmail.com>
2016-11-02 21:46:35 +0100

    Add new file

--- script.py
+++ script.py
@@ -3,14 +3,27 @@
 # Script fot searching free places at Brno VUT acomodation system,
 # it working only from inside network.
 
-import irllib2
+import urllib2
 
 url = 'https://www.kn.vutbr.cz/search/index.html?str='
+
+build = "B07"
+room = "1216"
+
+url = url + build + "-" + room
 
 response = urllib2.urlopen(url)
 webContent = response.read()
 
-print(webContent[0:300])
+matches = re.findall("Jméno:",webContent)
+
+print("Počet obsazených pokojů:")
+print(matches)
+
+# print(url)
+# print(webContent[0:9000])
+ 
+
 
 """
 from urllib2 import urlopen
